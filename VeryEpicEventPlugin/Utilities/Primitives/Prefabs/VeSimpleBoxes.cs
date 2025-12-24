61aea2d954d341731bb039f5b844edb1ec96dac4
MSaska <martin.saska@student.sps-cl.cz>
2025-12-24 17:39:39 +0100

    So like, there's a lot of changes that will need documentation and later explanation

--- VeryEpicEventPlugin/Utilities/Primitives/Prefabs/VeSimpleBoxes.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace VeryEpicEventPlugin.Utilities.Primitives.Prefabs;
-
-public class VeSimpleBoxes : VePrefabBase<VeSimpleBoxes>
-{
-    public override string PrefabName { get; } = "Simple Boxes Open Connector";
-}
